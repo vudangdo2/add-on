3f68f1828739deff984a710a575c5ff32c169bb6
vudangdo2 <dovd0991@gmail.com>
2016-01-25 22:42:16 +0700

    Delete inject-v1.js

--- inject-v1.js
+++
@@ -1,8 +0,0 @@
-  function addVisit(mysite1Ex , userowner1Ex){
-                          $.ajax({
-                                    type: "POST",
-                                    url: "wreceive.php",
-                                    data: "data=" + mysite1Ex + "---" + '25566429' + "---0---" + userowner1Ex,
-                                    cache: false
-                                });
-            }
