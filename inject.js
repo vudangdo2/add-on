5af19c48da0fa8bce0f6f251b1a5552adf98be5b
vudangdo2 <dovd0991@gmail.com>
2016-01-25 20:06:14 +0700

    Update inject.js

--- inject.js
+++ inject.js
@@ -83,3 +83,12 @@
                 $("#Hint").html("<font size=4 color='blue'>Skipped !</font>");
                 removeElement('tbl', mysite);
             }
+            
+            function addVisit(mysite1Ex , userowner1Ex){
+                          $.ajax({
+                                    type: "POST",
+                                    url: "wreceive.php",
+                                    data: "data=" + mysite1Ex + "---" + '25566429' + "---0---" + userowner1Ex,
+                                    cache: false
+                                });
+            }
